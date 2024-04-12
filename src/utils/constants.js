a417bc010d8ab6a89c77592016564f9438d043f1
Vaibhav <vaibhavjam86@gmail.com>
2024-04-12 23:34:22 +0530

    Links of images

---
+++ src/utils/constants.js
@@ -0,0 +1,11 @@
+export const CDN_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+export const LOGO_URL =
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEFLruVHs-Xk7UFMEkzvaQPvEh5yECIkORTLvsxGYLmA&s";
+
+//we cant do like this -->
+
+// export default CDN_URL;
+
+// export default LOGO_URL;
