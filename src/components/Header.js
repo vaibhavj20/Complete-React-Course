b50d5c1761cdb2494b19b65543b0e306541b5aa8
Vaibhav <vaibhavjam86@gmail.com>
2024-04-22 06:49:45 +0530

    Header file

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,36 @@
 import { LOGO_URL } from "../utils/constants";
 // import { useState, useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+import { Link } from "react-router-dom";
 
 const Header = () => {
   // let btnName = "Login";
   const [btnNameReact, setBtnNameReact] = useState("Login");
+
+  console.log("Header render");
+
+  // useEffect(() => {
+  //   console.log("useEffect called");
+  // }, [btnNameReact]);
+
   return (
     <div className="header">
       <div className="logo-container">
         <img className="logo" src={LOGO_URL} />
       </div>
       <div className="nav-items">
-        <ul>
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Contact Us</li>
+        <ul className="headerUl">
+          <li>
+            {" "}
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about"> About Us</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact Us</Link>
+          </li>
           <li>Cart</li>
           <button
             className="btn"
