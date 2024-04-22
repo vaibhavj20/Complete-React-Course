a4f10ab084f8bc8e68d069d7a7dd6ea8b452aaa5
Vaibhav <vaibhavjam86@gmail.com>
2024-04-22 06:41:39 +0530

    Added menu to the restaurant cards dynamically

---
+++ src/components/RestaurantMenu.js
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
+import { useParams } from "react-router-dom";
+import { MENU_API } from "../utils/constants";
+
+const RestaurantMenu = () => {
+  const [resInfo, setResInfo] = useState(null);
+  // const params = useParams();
+  // console.log(params);
+
+  const { resdId } = useParams();
+
+  useEffect(() => {
+    fetchMenu();
+  }, []);
+
+  const fetchMenu = async () => {
+    const data = await fetch(MENU_API + resdId);
+    console.log(MENU_API + resdId);
+    const json = await data.json();
+
+    console.log(json);
+    setResInfo(json.data);
+  };
+
+  if (resInfo === null) return <Shimmer />;
+
+  const { name, cuisines, costForTwoMessage } =
+    resInfo?.cards[2]?.card?.card?.info;
+
+  const { itemCards } =
+    resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
+
+  console.log(itemCards);
+
+  return (
+    <div className="menu">
+      <h1>{name}</h1>
+      <p>
+        {cuisines.join(",")} - {costForTwoMessage}
+      </p>
+      <h3>Menu</h3>
+
+      <ul>
+        {itemCards.map((item) => (
+          <li key={item.card.info.id}>
+            {item.card.info.name} - {"Rs."}
+            {item.card.info.price / 100 || item.card.info.defaultPrice / 100}
+          </li>
+        ))}
+        {/* <li>{itemCards[0].card.info.name}</li>
+        <li>{itemCards[1].card.info.name}</li>
+        <li>{itemCards[2].card.info.name}</li> */}
+      </ul>
+    </div>
+  );
+};
+
+export default RestaurantMenu;
