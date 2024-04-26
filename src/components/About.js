10badad7776406507dcd7269d842f2558095b753
Vaibhav <vaibhavjam86@gmail.com>
2024-04-26 11:01:20 +0530

    Adding user details

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,42 @@
 import React from "react";
+import User from "./User";
+import UserClass from "./UserClass";
 
-const About = () => {
-  return (
-    <div>
-      <h1>About</h1>
-      <h2>This is React Course</h2>
-    </div>
-  );
-};
+// const About = () => {
+//   return (
+//     <div>
+//       <h2>Hello Everyone,</h2>
+//       <User name={"Vaibhav Jamdhade(function)"} location={"Nashik(fn)"} />
+//       <UserClass name={"Vaibhav Jamdhade(Class)"} location={"Mumbai(class)"} />
+//     </div>
+//   );
+// };
+
+// export default About;
+
+class About extends React.Component {
+  constructor(props) {
+    super(props);
+    // console.log("Parent Constructor");
+  }
+
+  componentDidMount() {
+    // console.log("Parent Component Did Mounted");
+  }
+
+  render() {
+    // console.log("Parent Render");
+    return (
+      <div>
+        <h2>Hello Everyone,</h2>
+        {/* <User name={"Vaibhav Jamdhade(function)"} location={"Nashik(fn)"} /> */}
+        <UserClass
+          name={"Vaibhav Jamdhade(Class)"}
+          location={"Mumbai(class)"}
+        />
+      </div>
+    );
+  }
+}
 
 export default About;
