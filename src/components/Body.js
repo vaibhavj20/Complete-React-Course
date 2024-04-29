fa077c2c9f30f17e7219b8e83b120d0e5c3b0801
Vaibhav <vaibhavjam86@gmail.com>
2024-04-29 08:22:35 +0530

    Added online status logic

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,9 @@
 import RestaurantCard from "./RestaurantCard";
-
 // import { useEffect } from "react";
 import { useState, useEffect } from "react";
-
 import Shimmer from "./Shimmer";
 import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
 
 const Body = () => {
   //normal js variable
@@ -88,6 +87,14 @@ const Body = () => {
   // if (restaurantList.length === 0) {
   //   return <Shimmer />;
   // }
+
+  const onlineStatus = useOnlineStatus();
+
+  if (onlineStatus === false) {
+    return (
+      <h1>Looks like you're offline ! Please check your internet connection</h1>
+    );
+  }
 
   return restaurantList.length === 0 ? (
     <Shimmer />
