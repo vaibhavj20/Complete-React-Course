d2e3b67a4ec6d8484ae9e507a006d123ef0419b2
Vaibhav <vaibhavjam86@gmail.com>
2024-05-11 09:32:08 +0530

    Adding Tailwind CSS

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,27 +17,27 @@ const Header = () => {
   // }, [btnNameReact]);
 
   return (
-    <div className="header">
+    <div className="flex justify-between shadow-md ">
       <div className="logo-container">
-        <img className="logo" src={LOGO_URL} />
+        <img className="w-36 pl-[30px]" src={LOGO_URL} />
       </div>
-      <div className="nav-items">
-        <ul className="headerUl">
-          <li>Online Status:{onlineStatus ? "✅" : "🔴"}</li>
-          <li>
+      <div className="flex items-center">
+        <ul className="flex p-4 m-4">
+          <li className="p-4">Online Status:{onlineStatus ? "✅" : "🔴"}</li>
+          <li className="p-4">
             {" "}
             <Link to="/">Home</Link>
           </li>
-          <li>
+          <li className="p-4">
             <Link to="/about"> About Us</Link>
           </li>
-          <li>
+          <li className="p-4">
             <Link to="/contact">Contact Us</Link>
           </li>
-          <li>
+          <li className="p-4">
             <Link to="/grocery">Grocery</Link>
           </li>
-          <li>Cart</li>
+          <li className="p-4">Cart</li>
           <button
             className="btn"
             onClick={() => {
