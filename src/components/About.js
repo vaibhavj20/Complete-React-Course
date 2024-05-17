f92501ec9fb7167acf9b3447ae3bb3952ca341e0
Vaibhav <vaibhavjam86@gmail.com>
2024-05-17 11:16:38 +0530

    Updating username using class-based component

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,7 @@
 import React from "react";
 import User from "./User";
 import UserClass from "./UserClass";
+import UserContext from "../utils/UserContext";
 
 // const About = () => {
 //   return (
@@ -30,6 +31,14 @@ class About extends React.Component {
       <div>
         <h2>Hello Everyone,</h2>
         {/* <User name={"Vaibhav Jamdhade(function)"} location={"Nashik(fn)"} /> */}
+        <div>
+          LoggedIn User
+          <UserContext.Consumer>
+            {({ loggedInUser }) => (
+              <h1 className="font-bold">{loggedInUser}</h1>
+            )}
+          </UserContext.Consumer>
+        </div>
         <UserClass
           name={"Vaibhav Jamdhade(Class)"}
           location={"Mumbai(class)"}
