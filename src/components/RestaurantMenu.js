08e091f61aa15f5dc2ffb1a3c5e8f69b1d8e02ba
Vaibhav <vaibhavjam86@gmail.com>
2024-05-17 11:21:25 +0530

    Displayed all the restaurant categories in the form of accordion

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -2,6 +2,8 @@
 import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
 import useRestaurantMenu from "../utils/useRestaurantMenu";
+import RestaurantCategory from "./RestaurantCategory";
+import { useState } from "react";
 
 const RestaurantMenu = () => {
   // const [resInfo, setResInfo] = useState(null);
@@ -11,6 +13,7 @@ const RestaurantMenu = () => {
   const { resdId } = useParams();
 
   const resInfo = useRestaurantMenu(resdId);
+  const [showIndex, setShowIndex] = useState(null);
 
   // useEffect(() => {
   //   fetchMenu();
@@ -33,27 +36,34 @@ const RestaurantMenu = () => {
   const { itemCards } =
     resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
 
-  console.log(itemCards);
+  // console.log(itemCards);
+  // console.log("Vaby");
+  // console.log(resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards);
+
+  const categories =
+    resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
+      (c) =>
+        c.card?.["card"]?.["@type"] ===
+        "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
+    );
+  console.log("printing categories");
+  console.log(categories);
 
   return (
-    <div className="menu">
-      <h1>{name}</h1>
-      <p>
+    <div className="text-center">
+      <h1 className="font-bold my-6 text-2xl">{name}</h1>
+      <p className="font-bold text-lg">
         {cuisines.join(",")} - {costForTwoMessage}
       </p>
-      <h3>Menu</h3>
-
-      <ul>
-        {itemCards.map((item) => (
-          <li key={item.card.info.id}>
-            {item.card.info.name} - {"Rs."}
-            {item.card.info.price / 100 || item.card.info.defaultPrice / 100}
-          </li>
-        ))}
-        {/* <li>{itemCards[0].card.info.name}</li>
-        <li>{itemCards[1].card.info.name}</li>
-        <li>{itemCards[2].card.info.name}</li> */}
-      </ul>
+      {categories.map((category, index) => (
+        //controlled components
+        <RestaurantCategory
+          key={category?.card?.card?.title}
+          data={category?.card?.card}
+          showItems={index === showIndex ? true : false}
+          setShowIndex={() => setShowIndex(index)}
+        />
+      ))}
     </div>
   );
 };
