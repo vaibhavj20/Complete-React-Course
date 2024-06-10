502d310055f23dc7f8b378ad3a2a4da05ba40d92
Vaibhav <vaibhavjam86@gmail.com>
2024-06-10 10:42:43 +0530

    Created filter component for buttons

---
+++ Projects-Using-React/Courses-Dashboard/src/components/Filter.js
@@ -0,0 +1,12 @@
+const Filter = (props) => {
+  let filterData = props.filterData;
+  return (
+    <div>
+      {filterData.map((data) => (
+        <button>{data.title}</button>
+      ))}
+    </div>
+  );
+};
+
+export default Filter;
