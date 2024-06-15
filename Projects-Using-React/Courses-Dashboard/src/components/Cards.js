2e251845191b44d0802fb91600dac246b684e70f
Vaibhav <vaibhavjam86@gmail.com>
2024-06-15 22:04:48 +0530

    Displaying all the cards fetch from api

--- Projects-Using-React/Courses-Dashboard/src/components/Cards.js
+++ Projects-Using-React/Courses-Dashboard/src/components/Cards.js
@@ -1,4 +1,39 @@
-const Cards = () => {
-  return <div></div>;
+import { useState } from "react";
+import Card from "./Card";
+
+const Cards = (props) => {
+  let courses = props.courses;
+  let category = props.category;
+
+  const [likedCourses, setLikedCourses] = useState([]);
+
+  function getCourses() {
+    if (category === "All") {
+      let allCourses = [];
+      Object.values(courses).forEach((array) => {
+        array.forEach((courseData) => {
+          allCourses.push(courseData);
+        });
+      });
+
+      return allCourses;
+    } else {
+      //mei sirf specific category ka array pass krunga
+      return courses[category];
+    }
+  }
+
+  return (
+    <div className="flex flex-wrap justify-center gap-4 mb-4">
+      {getCourses().map((course) => (
+        <Card
+          key={course.id}
+          course={course}
+          likedCourses={likedCourses}
+          setLikedCourses={setLikedCourses}
+        />
+      ))}
+    </div>
+  );
 };
 export default Cards;
