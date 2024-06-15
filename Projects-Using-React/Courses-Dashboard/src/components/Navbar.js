667169b4f9014861253889cd84336faccf156be9
Vaibhav <vaibhavjam86@gmail.com>
2024-06-15 21:58:49 +0530

    Adding Header

--- Projects-Using-React/Courses-Dashboard/src/components/Navbar.js
+++ Projects-Using-React/Courses-Dashboard/src/components/Navbar.js
@@ -1,8 +1,10 @@
 const Navbar = () => {
   return (
     <div>
-      <nav>
-        <h1>Top Courses</h1>
+      <nav className="bg-bgDark py-4">
+        <h1 className="text-3xl font-bold text-center text-white">
+          Top Courses
+        </h1>
       </nav>
     </div>
   );
