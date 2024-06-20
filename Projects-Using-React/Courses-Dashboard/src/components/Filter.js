f342d6bf4980da0c20d7863402f9ce6810206185
Vaibhav <vaibhavjam86@gmail.com>
2024-06-20 11:23:09 +0530

    updated filter

--- Projects-Using-React/Courses-Dashboard/src/components/Filter.js
+++ Projects-Using-React/Courses-Dashboard/src/components/Filter.js
@@ -11,7 +11,7 @@ const Filter = (props) => {
       {filterData.map((data) => (
         <button
           className={`
-            text-lg px-2 py-1 rounded-md font-medium text-white bg-black hover:bg-opacity-50 border-2 transition-all duration-300 
+            text-lg px-2 py-1 rounded-md font-medium text-white bg-black hover:bg-opacity-50 border-2 transition-all duration-300
             ${
               category === data.title
                 ? "bg-opacity-60 border-white"
