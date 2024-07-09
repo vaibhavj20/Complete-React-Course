33506b6db76c1636433b22107ae16fb71ef8e836
Vaibhav <vaibhavjam86@gmail.com>
2024-07-09 22:22:46 +0530

    shimmer ui

---
+++ Projects-Using-React/FilmFlicks/src/components/templates/TrendingShimmer.js
@@ -0,0 +1,62 @@
+import React from "react";
+
+const TrendingShimmer = () => {
+  return (
+    <div className="animate-pulse w-full h-full">
+      <div className="flex items-center bg-[#2b2a31] p-4  mb-4 h-[110px]">
+        <div className="ml-5 w-12 h-12 bg-[#454449] rounded-full"></div>
+        <div className="w-[190px] ml-6 h-10 bg-[#454449] rounded mr-5"></div>
+        <div className="w-[600px] ml-6 h-10 bg-[#454449] rounded  mr-6"></div>
+        <div className="ml-6 w-[200px] h-10 bg-[#454449] rounded"></div>
+        <div className="ml-6 w-[200px] h-10 bg-[#454449] rounded"></div>
+      </div>
+
+      <div className="container mx-auto p-10 lg:px-16">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-4">
+          <div className="w-60 h-96 bg-[#2b2a31] p-4 rounded-lg animate-pulse">
+            <div className="h-[85%] bg-[#454449] rounded"></div>
+            <div className="h-[10%] mt-4 bg-[#454449] rounded"></div>
+          </div>
+          <div className="w-60 h-96 bg-[#2b2a31] p-4 rounded-lg animate-pulse">
+            <div className="h-[85%] bg-[#454449] rounded"></div>
+            <div className="h-[10%] mt-4 bg-[#454449] rounded"></div>
+          </div>
+          <div className="w-60 h-96 bg-[#2b2a31] p-4 rounded-lg animate-pulse">
+            <div className="h-[85%] bg-[#454449] rounded"></div>
+            <div className="h-[10%] mt-4 bg-[#454449] rounded"></div>
+          </div>
+          <div className="w-60 h-96 bg-[#2b2a31] p-4 rounded-lg animate-pulse">
+            <div className="h-[85%] bg-[#454449] rounded"></div>
+            <div className="h-[10%] mt-4 bg-[#454449] rounded"></div>
+          </div>
+          <div className="w-60 h-96 bg-[#2b2a31] p-4 rounded-lg animate-pulse">
+            <div className="h-[85%] bg-[#454449] rounded"></div>
+            <div className="h-[10%] mt-4 bg-[#454449] rounded"></div>
+          </div>
+          <div className="w-60 h-96 bg-[#2b2a31] p-4 rounded-lg animate-pulse">
+            <div className="h-[85%] bg-[#454449] rounded"></div>
+            <div className="h-[10%] mt-4 bg-[#454449] rounded"></div>
+          </div>
+          <div className="w-60 h-96 bg-[#2b2a31] p-4 rounded-lg animate-pulse">
+            <div className="h-[85%] bg-[#454449] rounded"></div>
+            <div className="h-[10%] mt-4 bg-[#454449] rounded"></div>
+          </div>
+          <div className="w-60 h-96 bg-[#2b2a31] p-4 rounded-lg animate-pulse">
+            <div className="h-[85%] bg-[#454449] rounded"></div>
+            <div className="h-[10%] mt-4 bg-[#454449] rounded"></div>
+          </div>
+          <div className="w-60 h-96 bg-[#2b2a31] p-4 rounded-lg animate-pulse">
+            <div className="h-[85%] bg-[#454449] rounded"></div>
+            <div className="h-[10%] mt-4 bg-[#454449] rounded"></div>
+          </div>
+          <div className="w-60 h-96 bg-[#2b2a31] p-4 rounded-lg animate-pulse">
+            <div className="h-[85%] bg-[#454449] rounded"></div>
+            <div className="h-[10%] mt-4 bg-[#454449] rounded"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TrendingShimmer;
