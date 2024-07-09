563c4466597ce252ea15318f4b0aff2602c536f5
Vaibhav <vaibhavjam86@gmail.com>
2024-07-09 22:11:16 +0530

    Add routes for Popular, Movie, TV Shows, and People pages

--- Projects-Using-React/FilmFlicks/src/App.js
+++ Projects-Using-React/FilmFlicks/src/App.js
@@ -1,6 +1,10 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Trending from "./components/Trending";
+import Popular from "./components/Popular";
+import Movie from "./components/Movie";
+import TvShows from "./components/TvShows";
+import People from "./components/People";
 
 function App() {
   return (
@@ -8,6 +12,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/trending" element={<Trending />} />
+        <Route path="/popular" element={<Popular />} />
+        <Route path="/movie" element={<Movie />} />
+        <Route path="/tvShows" element={<TvShows />} />
+        <Route path="/peoples" element={<People />} />
       </Routes>
     </div>
   );
