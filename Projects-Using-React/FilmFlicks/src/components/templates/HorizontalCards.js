5f2df31707d9d361e49b2b04f482bfcc644414cb
Vaibhav <vaibhavjam86@gmail.com>
2024-07-11 23:06:34 +0530

    Added hover effect on cards

--- Projects-Using-React/FilmFlicks/src/components/templates/HorizontalCards.js
+++ Projects-Using-React/FilmFlicks/src/components/templates/HorizontalCards.js
@@ -1,77 +1,38 @@
-// import React from "react";
-// // import { Link } from "react-router-dom";
-// // import Dropdown from "./Dropdown";
-
-// const HorizontalCards = ({ data }) => {
-//   console.log(data);
-
-//   return (
-//     <div className="w-full flex overflow-y-hidden mb-5 p-5">
-//       {data.map((data, index) => (
-//         <div key={index} className="min-w-[17%] bg-zinc-900 mr-5 mb-5 ">
-//           <img
-//             className="w-full h-[45%] object-cover"
-//             src={`https://image.tmdb.org/t/p/original/${
-//               data.backdrop_path || data.profile_path || data.poster_path
-//             })`}
-//             alt=""
-//           />
-//           <div className=" text-white p-3 h-[55%]">
-//             {" "}
-//             <h1 className=" text-xl font-semibold ">
-//               {data.name ||
-//                 data.title ||
-//                 data.original_name ||
-//                 data.original_title}
-//             </h1>
-//             <p className=" ">
-//               {data.overview.slice(0, 50)}...
-//               <span className="text-zinc-500">more</span>
-//             </p>
-//           </div>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default HorizontalCards;
-
 import React from "react";
 import { Link } from "react-router-dom";
 
 const HorizontalCards = ({ data }) => {
-  //console.log(data);
-
   return (
     <div className="w-full flex overflow-x-auto mb-5 p-5 ">
-      {data.map((data, index) => (
+      {data.map((item, index) => (
         <Link
-          to={`/${data.media_type}/details/${data.id}`}
+          to={`/${item.media_type}/details/${item.id}`}
           key={index}
-          className="min-w-[17%] bg-zinc-900 mr-5 mb-5  overflow-hidden"
+          className="min-w-[18%]  bg-zinc-900 mr-5 mb-5 overflow-hidden relative"
+          style={{ transition: "transform 0.3s ease-in-out" }}
         >
           <img
             className="w-full h-[45%] object-cover"
             src={`https://image.tmdb.org/t/p/original/${
-              data.backdrop_path || data.profile_path || data.poster_path
+              item.backdrop_path || item.profile_path || item.poster_path
             }`}
             alt=""
           />
           <div className="text-white p-3 flex flex-col justify-between">
             <h1 className="text-lg font-bold mb-2 truncate">
-              {data.name ||
-                data.title ||
-                data.original_name ||
-                data.original_title}
+              {item.name ||
+                item.title ||
+                item.original_name ||
+                item.original_title}
             </h1>
             <p className="text-sm text-gray-300 leading-snug">
-              {data.overview.length > 100
-                ? `${data.overview.slice(0, 100)}...`
-                : data.overview}
+              {item.overview.length > 100
+                ? `${item.overview.slice(0, 100)}...`
+                : item.overview}
               <span className="text-zinc-500"> more</span>
             </p>
           </div>
+          <div className="absolute inset-0 bg-black opacity-0 hover:opacity-50 transition-opacity duration-300"></div>
         </Link>
       ))}
     </div>
