b8f563e177d68e0288f748e2f0a93d0342109d9f
Vaibhav <vaibhavjam86@gmail.com>
2024-07-11 23:09:18 +0530

    Updated file

--- Projects-Using-React/FilmFlicks/src/store/actions/movieActions.js
+++ Projects-Using-React/FilmFlicks/src/store/actions/movieActions.js
@@ -9,6 +9,7 @@ export const asyncloadmovie = (id) => async (dispatch, getState) => {
     const recommendations = await axios.get(`/movie/${id}/recommendations`);
     const similar = await axios.get(`/movie/${id}/similar`);
     const videos = await axios.get(`/movie/${id}/videos`);
+    const translations = await axios.get(`/movie/${id}/translations`);
     const watchproviders = await axios.get(`/movie/${id}/watch/providers`);
 
     let theUltimateDetails = {
@@ -16,11 +17,12 @@ export const asyncloadmovie = (id) => async (dispatch, getState) => {
       externalid: externalid.data,
       recommendations: recommendations.data.results,
       similar: similar.data.results,
+      translations: translations.data.translations.map((t) => t.english_name),
       videos: videos.data.results.find((m) => m.type === "Trailer"),
       watchproviders: watchproviders.data.results.IN,
     };
     dispatch(loadMovie(theUltimateDetails));
-    console.log(theUltimateDetails);
+    // console.log(theUltimateDetails);
   } catch (error) {
     console.log("Error :", error);
   }
