981a3c234da56fb4572968b5dc7b90490a5bc26d
Vaibhav <vaibhavjam86@gmail.com>
2024-07-11 22:43:09 +0530

    Improve the layout of movie details component

--- Projects-Using-React/FilmFlicks/src/components/MovieDetails.js
+++ Projects-Using-React/FilmFlicks/src/components/MovieDetails.js
@@ -1,48 +1,341 @@
-import React from "react";
-import { useDispatch } from "react-redux";
-import { Link, useNavigate, useParams } from "react-router-dom";
+// import React, { useEffect } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { Link, useLocation, useNavigate, useParams } from "react-router-dom";
+// import { asyncloadmovie, removeMovie } from "../store/actions/movieActions";
+// import HorizontalCards from "./templates/HorizontalCards";
+// import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+// import "react-circular-progressbar/dist/styles.css";
+// import DetailsShimmer from "./templates/DetailsShimmer";
+
+// const MovieDetails = () => {
+//   const { pathname } = useLocation();
+//   const { info } = useSelector((state) => state.movie);
+//   const navigate = useNavigate();
+//   const dispatch = useDispatch();
+//   const { id } = useParams();
+//   console.log(info);
+
+//   useEffect(() => {
+//     dispatch(asyncloadmovie(id));
+//     return () => {
+//       dispatch(removeMovie());
+//     };
+//   }, [id]);
+
+//   return info ? (
+//     <div
+//       style={{
+//         background: `linear-gradient(rgba(0,0,0,.4),rgba(0,0,0,.7),rgba(0,0,0,.9)), url(https://image.tmdb.org/t/p/original/${info.detail.backdrop_path})`,
+//         backgroundPosition: "center",
+//         backgroundSize: "cover",
+//         backgroundRepeat: "no-repeat",
+//       }}
+//       className="w-screen h-[140vh] flex flex-col items-center justify-start px-4 py-10 bg-black"
+//     >
+//       {/* Navigation */}
+//       <nav className="w-full max-w-7xl text-white flex justify-start items-center gap-4 mb-8">
+//         <button
+//           onClick={() => navigate(-1)}
+//           className="text-2xl font-bold p-2 hover:text-purple-400 ri-arrow-left-line cursor-pointer rounded-full bg-black bg-opacity-50"
+//         />
+//         <a
+//           target="_blank"
+//           rel="noopener noreferrer"
+//           href={info.detail.homepage}
+//           className="rounded-full bg-black bg-opacity-50 p-2"
+//         >
+//           <i className="ri-external-link-fill text-2xl hover:text-purple-400"></i>
+//         </a>
+//         <a
+//           target="_blank"
+//           rel="noopener noreferrer"
+//           href={`https://www.wikidata.org/wiki/${info.externalid.wikidata_id}`}
+//           className="rounded-full bg-black bg-opacity-50 p-2"
+//         >
+//           <i className="ri-earth-fill text-2xl hover:text-purple-400"></i>
+//         </a>
+//         <a
+//           target="_blank"
+//           rel="noopener noreferrer"
+//           href={`https://www.imdb.com/title/${info.externalid.imdb_id}/`}
+//           className="rounded-full bg-black bg-opacity-50 p-2"
+//         >
+//           <p className="font-bold hover:text-purple-400">IMDb</p>
+//         </a>
+//       </nav>
+
+//       {/* Movie Details Container */}
+//       <div className="w-full max-w-7xl  bg-black bg-opacity-70 p-8 rounded-lg flex flex-col md:flex-row items-start gap-8 text-white relative">
+//         <img
+//           className="shadow-lg rounded-lg h-96 md:h-[50vh] object-cover"
+//           src={`https://image.tmdb.org/t/p/original/${
+//             info.detail.poster_path || info.detail.backdrop_path
+//           }`}
+//           alt=""
+//         />
+//         <div className="flex flex-col space-y-4">
+//           <h1 className="text-5xl font-black">
+//             {info.detail.name ||
+//               info.detail.title ||
+//               info.detail.original_name ||
+//               info.detail.original_title}
+//             <span className="text-2xl font-bold text-zinc-300 ml-2">
+//               ({info.detail.release_date.split("-")[0]})
+//             </span>
+//           </h1>
+//           <h1 className="text-xl font-semibold italic text-zinc-300">
+//             {info.detail.tagline}
+//           </h1>
+//           <div className="flex flex-wrap items-center gap-1">
+//             <div className="w-14 h-14">
+//               <CircularProgressbar
+//                 value={info.detail.vote_average * 10}
+//                 text={`${(info.detail.vote_average * 10).toFixed()}%`}
+//                 styles={buildStyles({
+//                   textSize: "28px",
+//                   textColor: "#fff",
+//                   pathColor: "#f8e71c",
+//                   trailColor: "#d6d6d6",
+//                   textStyle: {
+//                     fontWeight: "bold",
+//                   },
+//                 })}
+//                 className="hover:scale-110 transition-transform duration-300"
+//               />
+//             </div>
+//             <div className="flex flex-col items-center">
+//               <span className="font-semibold text-lg">User</span>
+//               <span className="font-semibold text-lg">Score</span>
+//             </div>
+//             <h1 className="ml-4 flex items-center text-zinc-200">
+//               {info.detail.genres.map((g) => g.name).join(", ")}
+//               <span className="mx-2">•</span>
+//               {info.detail.release_date}
+//               <span className="mx-2">•</span>
+//               {info.detail.runtime} min
+//             </h1>
+//           </div>
+//           <h1 className="text-2xl">Overview</h1>
+//           <hr className="text-zinc-500" />
+//           <p className="mb-10">{info.detail.overview}</p>
+//           <Link
+//             className="font-bold p-3 w-[20%] rounded bg-purple-600 hover:bg-purple-700 text-white flex items-center"
+//             to={`${pathname}/trailer`}
+//           >
+//             <i className="ri-play-fill mr-2"></i> Play Trailer
+//           </Link>
+//         </div>
+//         <div className="absolute top-8 right-8 bg-[#40cd2a66] text-white font-extrabold py-2 px-4 rounded shadow-lg">
+//           {info.detail.status}
+//         </div>
+//       </div>
+
+//       {/* Recommendations */}
+//       <hr className="mt-10 mb-5 border-none h-0.5 bg-zinc-200" />
+//       <h1 className="text-3xl font-semibold text-white mb-5 text-left">
+//         Recommendations
+//       </h1>
+//       <div className="w-full max-w-7xl">
+//         <HorizontalCards
+//           data={
+//             info.recommendations.length > 0
+//               ? info.recommendations
+//               : info.similar
+//           }
+//         />
+//       </div>
+//     </div>
+//   ) : (
+//     // <h1 className="text-white text-center mt-20 text-3xl">Loading...</h1>
+//     <DetailsShimmer />
+//   );
+// };
+
+// export default MovieDetails;
+
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useLocation, useNavigate, useParams } from "react-router-dom";
 import { asyncloadmovie, removeMovie } from "../store/actions/movieActions";
-import { useEffect } from "react";
+import HorizontalCards from "./templates/HorizontalCards";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+import DetailsShimmer from "./templates/DetailsShimmer";
 
 const MovieDetails = () => {
+  const { pathname } = useLocation();
+  const { info } = useSelector((state) => state.movie);
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const { id } = useParams();
+  console.log(info);
 
   useEffect(() => {
     dispatch(asyncloadmovie(id));
     return () => {
       dispatch(removeMovie());
     };
-  });
+  }, [id]);
 
-  return (
+  return info ? (
     <div
       style={{
-        background: `linear-gradient(rgba(0,0,0,.4),rgba(0,0,0,.7),rgba(0,0,0,.9)) ,url(https://image.tmdb.org/t/p/original/${
-          data.backdrop_path || data.profile_path || data.poster_path
-        })`,
+        background: `linear-gradient(rgba(0,0,0,.4),rgba(0,0,0,.7),rgba(0,0,0,.9)), url(https://image.tmdb.org/t/p/original/${info.detail.backdrop_path})`,
         backgroundPosition: "center",
         backgroundSize: "cover",
         backgroundRepeat: "no-repeat",
       }}
-      className="w-screen h-screen px-[10%]"
+      className="w-screen h-[140vh] flex flex-col items-center justify-start px-4 py-10 bg-black"
     >
-      <nav className="w-full text-zinc-400">
-        <Link
+      {/* Navigation */}
+      <nav className="w-full max-w-7xl text-white flex justify-start items-center gap-4 mb-8">
+        <button
           onClick={() => navigate(-1)}
-          className=" text-[19px] font-bold text-white p-3 hover:text-purple-400 ri-arrow-left-line cursor-pointer"
-        ></Link>
-        <a href="">
-          <i class="ri-external-link-fill"></i>
+          className="text-2xl font-bold p-2 hover:text-purple-400 ri-arrow-left-line cursor-pointer rounded-full bg-black bg-opacity-50"
+        />
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href={info.detail.homepage}
+          className="rounded-full bg-black bg-opacity-50 p-2"
+        >
+          <i className="ri-external-link-fill text-2xl hover:text-purple-400"></i>
         </a>
-        <a href="">
-          <i class="ri-earth-fill"></i>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href={`https://www.wikidata.org/wiki/${info.externalid.wikidata_id}`}
+          className="rounded-full bg-black bg-opacity-50 p-2"
+        >
+          <i className="ri-earth-fill text-2xl hover:text-purple-400"></i>
         </a>
-
-        <a href="">imdb</a>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href={`https://www.imdb.com/title/${info.externalid.imdb_id}/`}
+          className="rounded-full bg-black bg-opacity-50 p-2"
+        >
+          <p className="font-bold hover:text-purple-400">IMDb</p>
+        </a>
       </nav>
+
+      {/* Movie Details Container */}
+      <div className="w-full max-w-7xl bg-black bg-opacity-70 p-8 rounded-lg flex flex-col md:flex-row items-start gap-8 text-white relative">
+        <img
+          className="shadow-lg rounded-lg h-96 md:h-[50vh] object-cover"
+          src={`https://image.tmdb.org/t/p/original/${
+            info.detail.poster_path || info.detail.backdrop_path
+          }`}
+          alt=""
+        />
+        <div className="flex flex-col space-y-4 flex-grow">
+          <h1 className="text-5xl font-black">
+            {info.detail.name ||
+              info.detail.title ||
+              info.detail.original_name ||
+              info.detail.original_title}
+            <span className="text-2xl font-bold text-zinc-300 ml-2">
+              ({info.detail.release_date.split("-")[0]})
+            </span>
+          </h1>
+          <h1 className="text-xl font-semibold italic text-zinc-300">
+            {info.detail.tagline}
+          </h1>
+          <div className="flex flex-wrap items-center gap-1">
+            <div className="w-14 h-14">
+              <CircularProgressbar
+                value={info.detail.vote_average * 10}
+                text={`${(info.detail.vote_average * 10).toFixed()}%`}
+                styles={buildStyles({
+                  textSize: "28px",
+                  textColor: "#fff",
+                  pathColor: "#f8e71c",
+                  trailColor: "#d6d6d6",
+                  textStyle: {
+                    fontWeight: "bold",
+                  },
+                })}
+                className="hover:scale-110 transition-transform duration-300"
+              />
+            </div>
+            <div className="flex flex-col items-center">
+              <span className="font-semibold text-lg">User</span>
+              <span className="font-semibold text-lg">Score</span>
+            </div>
+            <h1 className="ml-4 flex items-center text-zinc-200">
+              {info.detail.genres.map((g) => g.name).join(", ")}
+              <span className="mx-2">•</span>
+              {info.detail.release_date}
+              <span className="mx-2">•</span>
+              {info.detail.runtime} min
+            </h1>
+          </div>
+          <h1 className="text-2xl">Overview</h1>
+          <hr className="text-zinc-500" />
+          <p className="mb-10">{info.detail.overview}</p>
+          <Link
+            className="font-bold p-3 w-[20%] rounded bg-purple-600 hover:bg-purple-700 text-white flex items-center"
+            to={`${pathname}/trailer`}
+          >
+            <i className="ri-play-fill mr-2"></i> Play Trailer
+          </Link>
+        </div>
+        <div className="absolute top-8 right-8 bg-[#40cd2a66] text-white font-extrabold py-2 px-4 rounded shadow-lg">
+          {info.detail.status}
+        </div>
+
+        {/* Social Media Links */}
+        <div className="absolute bottom-8 right-8 flex space-x-4">
+          {info.externalid.facebook_id && (
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href={`https://www.facebook.com/${info.externalid.facebook_id}`}
+              className="rounded-full bg-black bg-opacity-50 p-2"
+            >
+              <i className="ri-facebook-fill text-2xl hover:text-blue-600"></i>
+            </a>
+          )}
+          {info.externalid.instagram_id && (
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href={`https://www.instagram.com/${info.externalid.instagram_id}`}
+              className="rounded-full bg-black bg-opacity-50 p-2"
+            >
+              <i className="ri-instagram-fill text-2xl hover:text-pink-500"></i>
+            </a>
+          )}
+          {info.externalid.twitter_id && (
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href={`https://twitter.com/${info.externalid.twitter_id}`}
+              className="rounded-full bg-black bg-opacity-50 p-2"
+            >
+              <i className="ri-twitter-fill text-2xl hover:text-blue-400"></i>
+            </a>
+          )}
+        </div>
+      </div>
+
+      {/* Recommendations */}
+      <hr className="mt-10 mb-5 border-none h-0.5 bg-zinc-200" />
+      <h1 className="text-3xl font-semibold text-white mb-5 text-left">
+        Recommendations
+      </h1>
+      <div className="w-full max-w-7xl">
+        <HorizontalCards
+          data={
+            info.recommendations.length > 0
+              ? info.recommendations
+              : info.similar
+          }
+        />
+      </div>
     </div>
+  ) : (
+    <DetailsShimmer />
   );
 };
 
