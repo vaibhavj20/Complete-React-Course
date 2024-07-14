c3cc1101256040bd4f2abc99e6bc45bea985f0ce
Vaibhav <vaibhavjam86@gmail.com>
2024-07-14 09:57:17 +0530

    update: Created child routes for movie to show its trailer

--- Projects-Using-React/FilmFlicks/src/App.js
+++ Projects-Using-React/FilmFlicks/src/App.js
@@ -8,7 +8,8 @@ import People from "./components/People";
 import MovieDetails from "./components/MovieDetails";
 import TvDetails from "./components/TvDetails";
 import PersonDetails from "./components/PersonDetails";
-
+import Trailer from "./components/templates/Trailer";
+import NotFound from "./components/templates/NotFound";
 function App() {
   return (
     <div className="bg-[#1F1E24] w-screen h-screen flex">
@@ -17,11 +18,17 @@ function App() {
         <Route path="/trending" element={<Trending />} />
         <Route path="/popular" element={<Popular />} />
         <Route path="/movie" element={<Movie />} />
-        <Route path="/movie/details/:id" element={<MovieDetails />} />
+        <Route path="/movie/details/:id" element={<MovieDetails />}>
+          <Route
+            path="/movie/details/:id/trailer"
+            element={<Trailer />}
+          ></Route>
+        </Route>
         <Route path="/tv" element={<TvShows />} />
         <Route path="/tv/details/:id" element={<TvDetails />} />
         <Route path="/person" element={<People />} />
         <Route path="/person/details/:id" element={<PersonDetails />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
