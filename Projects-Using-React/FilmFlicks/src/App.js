82109e221cd4c01a2476367a23e0a618381daa6a
Vaibhav <vaibhavjam86@gmail.com>
2024-07-15 22:28:50 +0530

    update: Added path for not found pages

--- Projects-Using-React/FilmFlicks/src/App.js
+++ Projects-Using-React/FilmFlicks/src/App.js
@@ -14,7 +14,7 @@ function App() {
   return (
     <div className="bg-[#1F1E24] w-screen h-screen flex">
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />}></Route>
         <Route path="/trending" element={<Trending />} />
         <Route path="/popular" element={<Popular />} />
         <Route path="/movie" element={<Movie />} />
@@ -25,7 +25,9 @@ function App() {
           ></Route>
         </Route>
         <Route path="/tv" element={<TvShows />} />
-        <Route path="/tv/details/:id" element={<TvDetails />} />
+        <Route path="/tv/details/:id" element={<TvDetails />}>
+          <Route path="/tv/details/:id/trailer" element={<Trailer />}></Route>
+        </Route>
         <Route path="/person" element={<People />} />
         <Route path="/person/details/:id" element={<PersonDetails />} />
         <Route path="*" element={<NotFound />} />
